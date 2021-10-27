882e5163ec8b39a18b67211cc1a0f8cce8118aa9
YellowFlash2012 <cyclop9@hotmail.com>
2021-10-27 10:34:19 +0100

    another change to app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Yo, I'm back!"
+    return "Yo, this is to test staging and prod pipeline!"
 
 #if __name__ == "__main__":
 # app.run(debug=True)
