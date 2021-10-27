713661e24cd08ff221ac0a38f72b1fbca92aa682
YellowFlash2012 <cyclop9@hotmail.com>
2021-10-27 13:43:09 +0100

    created config file and updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route('/')
 def home():
-    return "Yo, this is to test staging and prod pipeline!"
+    secret_key = app.config.get("SECRET_KEY")
+    return f"The configured secret key is {secret_key}."
 
 #if __name__ == "__main__":
-# app.run(debug=True)
+    #app.run(debug=True)
