5d86d0ec5ee9dab0420fd34ba444cf9789d70f4c
YellowFlash2012 <cyclop9@hotmail.com>
2021-10-30 14:45:06 +0100

    created the wordcount functionality, created index.html and updated the corresponding route

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
+import requests, operator, re, nltk
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
 
 from flask_migrate import Migrate
 from dotenv import *
@@ -16,13 +20,54 @@ migrate = Migrate(app, db)
 
 from models import *
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Yo, how you doing?"
+    errors = []
+    results = {}
+    if request.method == "POST":
+        #get the url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            
+        except:
+            errors.append("Unable to get the url. Please try again!")
+            return render_template('index.html', errors=errors)
+
+        if r:
+            #text processing
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            nltk.data.path.append('./nltk_data/') #set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            #remove punctuation, count raw words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+            #stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+            #save the results
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )[:10]
+            try:
+                result = Result(
+                    url = url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
+    return render_template('index.html', errors=errors, results=results)
 
 @app.route('/<name>')
 def hello_name(name):
     return "Yo, {}!".format(name)
 
-#if __name__ == "__main__":
-    #app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
